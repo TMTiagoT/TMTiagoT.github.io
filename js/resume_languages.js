7bea699e4a956edbfbbfb5f362500da20c92ccd5
TMTiagoT <tiagosmi@hotmail.com>
2019-12-26 15:04:02 -0300

    Added translation to portuguese and some fixes (probably final version v1)

---
+++ js/resume_languages.js
@@ -0,0 +1,202 @@
+var language_texts = {
+
+	en_us: { //english
+	//    welcome: "Welcome!",
+		about_navbar: "About",
+		education_navbar: "Education",
+		experience_navbar: "Experience",
+		major_projects_navbar: "Major Projects",
+		skills_navbar: "Skills",
+		events_courses_navbar: "Events &#38; Courses",
+		interests_navbar: "Interests",
+		
+		my_data: "Cell: [+55] (11) 995988-7910 · Mail: <a href='mailto:tiago.silva.miranda@usp.br'>tiago.silva.miranda@usp.br</a>",
+		my_about: "I am a student of computer science with major skills in back-end development and data engineering.<br/>I have worked, during my graduation, in projects of general software development, database development and exploration, game development, and so forth.",
+		
+		education_section: "Education",
+		
+		usp_title: "University of Sao Paulo",
+		usp_course: "Bachelor of Computer Science",
+		usp_description: "Bachelor’s degree in Computer Science, along with emphasis in Data Engineering.",
+		usp_note: "<b>Note:</b> To achieve an emphasis is necessary to do extra classes, besides the computer science bachelor default classes.",
+		usp_gpa: "<b>GPA:</b> 8.8/10",
+		usp_location: "Sao Carlos, Brazil",
+		usp_date: "Jan. 2016 - Dec. 2020 (expected)",
+		
+		ifsp_title: "Federal Institute of Sao Paulo",
+		ifsp_course: "High School with Computer Technician",
+		ifsp_description: "Standard high school classes, along with computer technician classes.",
+		ifsp_institute: "IFSP - Campus Sao Paulo",
+		ifsp_location: "Sao Paulo, Brazil",
+		ifsp_date: "Jan. 2012 - Dec. 2015",
+		
+		experience_section: "Experience",
+		fog_description: "FoG is an extension group of ICMC-USP focused on game development and knowledge dissemination through courses and events open to the public.",
+		fog_member: "<b>Member:</b> Engaged in the group activities since 2017, focused on back-end development of games and assets.",
+		fog_date: "Jan. 2017 - Present",
+		
+		pcg_title: "Scientific Research - PCG",
+		pcg_description: "A Scientific Research in Procedural Content Generation for platform games. This research aims the generation of two-dimensional (2D) caves, using the algorithms Perlin Noise and Cellular Automata as base.",
+		pcg_date: "Jan. 2019 - Present",
+		
+		major_projects_section: "Major Projects",
+		major_projects_description: "In this section, only a sample of the major projects developed will be shown.",
+		
+		mush_description: "A fast-paced 2D arena brawler game with two players, where each one choose a character with different abilities, and fight in a battlefield of their choice.",
+		mush_development: "<b>Development:</b> Responsible for the back-end development of scenario hazards, players movement and attacks, and for the integration of the animations.",
+		mush_date: "Aug. 2018 - Dec. 2018",
+		
+		database_development_title: "Database Development",
+		database_development_description: "Development of a normalized and consistent database and an application to manage buffets of proms and weddings.",
+		database_development_development: "<b>Development:</b> Responsible for database management and construction of triggers in Oracle SQL, in addition to the database connection and front-end application in Java.",
+		database_development_link: "DB Project github.com",
+		
+		entretenibit_description: "An application that automatically scans the web searching for events in a specific city and display’s it for the user in a website. It also sends a weekly newsletter with content the user is interested in.",
+		entretenibit_development: "<b>Development:</b> Responsible for creating the front-end using React.js.",
+		entretenibit_methodology: "<b>Methodology:</b> Agile methodology (SCRUM).",
+		
+		flocking_title: "Flocking Simulation",
+		flocking_description: "A simulation of the flocking behavior of birds or fishes, as known as Boids. The final goal was to use evolutionary algorithms to achieve better behaviors for the flocks.",
+		flocking_development: "<b>Development:</b> Responsible for the development of the flocking behaviors and for optimizing the code using the new technology of Unity Engine, called DOTS (high-performance multithreaded Data-Oriented Technology Stack).",
+		flocking_link: "Flocking Simulation github.com",
+		flocking_date: "Aug. 2019 - Dec. 2019",
+		
+		warehousing_description: "Development of a data warehouse of an university to track students performance. The work consisted of modeling and documentating the problem, including schematisation and ETL (Extract, Transform, Load). In addition to the implementation and loading of the database and some queries.",
+		warehousing_development: "<b>Development:</b> This work was produced mostly in group. Responsible for the database implementation and load, and queries implementation.",
+		warehousing_date: "Aug. 2019 - Dec. 2019",
+		
+		skills_section: "Skills",
+		
+		skills_programming: "Programming",
+		skills_tools: "Tools",
+		
+		events_courses_section: "Events &#38; Courses",
+		
+		events_courses_events: "Events",
+		event_semcomp: "Participant of Semcomp 19 to 22 (2016 - 2019)",
+		event_unity_user_group: "Participant of Unity User Group May 2018 and November 2018",
+		event_usp_game_link: "Participant of USP Game Link November 2018",
+		
+		events_courses_courses: "Courses",
+		course_musical_programming_chuck: "Minicourse of Musical Programming with Chuck (Semcomp 2016)",
+		course_game_developing_unity: "Minicourses of Game Developing with Unity (Semcomp - ICMC-USP)",
+		course_introduction_machine_learning: "Minicourse of Introduction to Machine Learning (Semcomp 2018)",
+		course_deep_learning: "Minicourse of Deep Learning (Semcomp)",
+		course_data_acquisition_analysis: "Minicourse of Data Acquisition and Exploratory Analysis (Semcomp 2019)",
+		course_data_visualization_tableau: "Minicourse of Data Visualization on Tableau (Semcomp 2019)",
+		
+		interests_section: "Interests",
+		interests_description_first: "As has been said before, I am a computer science student and my main interests are in the fields of Game Development and Databases. Besides that, during my spare time, I do like to play some games and watch some content around the internet (like YouTube videos, including Anime fights and others). In addition, I like developing games and related stuff.",
+		interests_description_second: "As simple as it seems what was said, I have done some things during my life until now, such as read some books, English and Japanese classes, Piano classes, and even Swimming ones. At last, but not less important, I do like to finish the things that I started, when possible.",
+		
+		//a: ""
+	}, 
+
+	pt_br: { //portuguese
+	//    welcome: "Bem Vindo!",
+		about_navbar: "Sobre",
+		education_navbar: "Educação",
+		experience_navbar: "Experiência",
+		major_projects_navbar: "Principais Projetos",
+		skills_navbar: "Conhecimentos",
+		events_courses_navbar: "Eventos &#38; Cursos",
+		interests_navbar: "Interesses",
+		
+		my_data: "Cel: [+55] (11) 995988-7910 · Email: <a href='mailto:tiago.silva.miranda@usp.br'>tiago.silva.miranda@usp.br</a>",
+		my_about: "Sou um estudante de ciência da computação com habilidades principalmente nas áreas de desenvolvimento back-end e engenharia de dados.<br/>Trabalhei, durante a minha graduação, em projetos de desenvolvimento de software, desenvolvimento e exploração de banco de dados, desenvolvimento de jogos, entre outros.",
+		
+		education_section: "Educação",
+		usp_title: "Universidade de São Paulo",
+		usp_course: "Bacharelado em Ciência da Computação",
+		usp_description: "Bacharelado em ciência da computação, juntamente com ênfase em engenharia de dados",
+		usp_note: "<b>Nota:</b> Para adquirir uma ênfase é necessário cursar matérias extras, além das matérias padrão do curso.",
+		usp_gpa: "<b>Média de Notas:</b> 8.8/10",
+		usp_location: "São Carlos, Brasil",
+		usp_date: "Jan. 2016 - Dez. 2020 (esperado)",
+		
+		ifsp_title: "Instituto Federal de São Paulo",
+		ifsp_course: "Ensino Médio integrado ao Técnico em Informática",
+		ifsp_description: "Aulas padrão de ensino médio, juntamente com aulas de técnico em informática.",
+		ifsp_institute: "IFSP - Campus São Paulo",
+		ifsp_location: "São Paulo, Brasil",
+		ifsp_date: "Jan. 2012 - Dez. 2015",
+		
+		experience_section: "Experiência",
+		fog_description: "O FoG é um grupo de extensão do ICMC-USP focado no desenvolvimento de jogos e na disseminação de conhecimento através de cursos e eventos abertos ao público.",
+		fog_member: "<b>Membro:</b> Envolvido nas atividades do grupo desde 2017, focado no desenvolvimento de jogos e assets.",
+		fog_date: "Jan. 2017 - Atual",
+		
+		pcg_title: "Iniciação Científica - PCG",
+		pcg_description: "Uma Iniciação Científica em Geração Procedural de Conteúdo para jogos de plataforma. Essa pesquisa visa a geração de cavernas em duas dimensões (2D), utilizando como base os algoritmos Perlin Noise e Autômato Celular.",
+		pcg_date: "Jan. 2019 - Atual",
+		
+		major_projects_section: "Principais Projetos",
+		major_projects_description: "Nesta seção, apenas uma parte dos principais projetos desenvolvidos será exibida.",
+		
+		mush_description: "Um jogo 2D de luta em arena de ritmo acelerado com dois jogadores, onde cada um escolhe um personagem com diferentes habilidades, e lutam no campo de batalha de sua escolha.",
+		mush_development: "<b>Desenvolvimento:</b> Responsável pelo desenvolvimento do back-end dos perigos de cenário, movimento e ataques dos jogadores, e pela integração das animações.",
+		mush_date: "Ago. 2018 - Dez. 2018",
+		
+		database_development_title: "Desenvolvimento de Base de Dados",
+		database_development_description: "Desenvolvimento de uma base de dados normalizada e consistente e de um aplicativo para gerenciar buffets de formaturas e casamentos.",
+		database_development_development: "<b>Desenvolvimento:</b> Responsável pelo gerenciamento da base de dados e pela criação de gatilhos no Oracle SQL, e pela conexão com o banco de dados e o front-end do aplicativo em Java.",
+		database_development_link: "Projeto de BD github.com",
+		
+		entretenibit_description: "Um aplicativo que varre automaticamente a web em busca de eventos de uma cidade específica e os exibe para o usuário em um website. Também envia um boletim informativo com o conteúdo no qual o usuário está interessado.",
+		entretenibit_development: "<b>Desenvolvimento:</b> Responsável pela criação do front-end utilizando React.js.",
+		entretenibit_methodology: "<b>Metodologia:</b> Metodologia Ágil (SCRUM).",
+		
+		flocking_title: "Flocking Simulation", //"Simulação de Bando",
+		flocking_description: "Uma simulação do comportamento de bando de pássaros ou peixes, também conhecida como Boids. O objetivo final era utilizar algoritmos evolutivos para obter melhores comportamentos para os bandos.",
+		flocking_development: "<b>Desenvolvimento:</b> Responsável pelo desenvolvimento dos comportamentos de bando e pela otimização do código utilizando a nova tecnologia da Unity Engine, chamada DOTS (Data-Oriented Technology Stack multithread de alto desempenho).",
+		flocking_link: "Simulação de Bando github.com",
+		flocking_date: "Ago. 2019 - Dez. 2019",
+		
+		warehousing_description: "Desenvolvimento de um armazém de dados de uma universidade para acompanhar o desempenho dos alunos. O trabalho consistiu em modelar e documentar o problema, incluindo a esquematização e o ETL (Extração, Transformação, Carregamento). Juntamente com a implementação e o carregamento do banco de dados e de algumas consultas.",
+		warehousing_development: "<b>Desenvolvimento:</b> Este trabalho foi desenvolvido principalmente em grupo. Responsável pela implementação e carregamento do banco de dados e pela implementação das consultas.",
+		warehousing_date: "Ago. 2019 - Dez. 2019",
+		
+		skills_section: "Conhecimentos",
+		
+		skills_programming: "Programação",
+		skills_tools: "Ferramentas",
+		
+		events_courses_section: "Eventos &#38; Cursos",
+		
+		events_courses_events: "Eventos",
+		event_semcomp: "Participante das Semcomp 19 à 22 (2016 - 2019)",
+		event_unity_user_group: "Participante do Unity User Group em Maio de 2018 e Novembro de 2018",
+		event_usp_game_link: "Participante do USP Game Link em Novembro de 2018",
+		
+		events_courses_courses: "Cursos",
+		course_musical_programming_chuck: "Minicurso de Programação Musical com Chuck (Semcomp 2016)",
+		course_game_developing_unity: "Minicursos de Desenvolvimento de Jogos com Unity (Semcomp - ICMC-USP)",
+		course_introduction_machine_learning: "Minicurso de Introdução à Machine Learning (Semcomp 2018)",
+		course_deep_learning: "Minicurso de Deep Learning (Semcomp)",
+		course_data_acquisition_analysis: "Minicurso de Aquisição e Análise Exploratória de Dados (Semcomp 2019)",
+		course_data_visualization_tableau: "Minicurso de Visualização de Dados no Tableau (Semcomp 2019)",
+		
+		interests_section: "Interesses",
+		interests_description_first: "Como já foi dito antes, sou um estudante de ciência da computação e meus principais interesses estão nas áreas de Desenvolvimento de Jogos e Bancos de Dados. Fora isso, durante meu tempo livre, gosto de jogar alguns jogos e assistir a conteúdos pela internet (como vídeos do YouTube, incluindo Anime fights, entre outros). Além disso, gosto de desenvolver jogos e coisas relacionadas.",
+		interests_description_second: "Por mais simples que pareça o que foi dito, já fiz algumas coisas durante a minha vida até agora, como ler alguns livros, aulas de inglês e japonês, aulas de piano e até natação. Por fim, mas não menos importante, gosto de terminar as coisas que começo, quando possível.",
+		
+		//a: ""
+	}
+};
+
+var language_toggle = document.getElementById("language_toggle"); //initialize vars
+
+// set all the text
+//document.getElementById("welcome").innerHTML = lang.welcome;
+set_allText(); //set all texts based on language selected
+
+function set_allText() { //set all texts based on language selected
+	var lang = language_toggle.checked ? "pt_br" : "en_us";
+	var lang_texts = language_texts[lang];
+	
+	for (var id in lang_texts) {
+		var page_element = document.getElementById(id);
+		if(page_element) page_element.innerHTML = lang_texts[id];
+		else console.log("Element with id (" + id + ") not found!");
+	}
+}
